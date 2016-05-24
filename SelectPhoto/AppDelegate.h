a5882710d3c8ada40152e21d99f525354f8ee141
CoderFan <447690182@qq.com>
2016-05-24 16:58:42 +0800

    Initial Commit

---
+++ SelectPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectPhoto
+//
+//  Created by jadefan on 16/5/24.
+//  Copyright © 2016年 jadefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectPhoto
+//
+//  Created by jadefan on 16/5/24.
+//  Copyright © 2016年 jadefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
